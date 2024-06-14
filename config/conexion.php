ff27d009fd0bb4ba661ab93c27a5441253e73fee
luzmarina23 <142471155+luzmarina23@users.noreply.github.com>
2024-06-14 17:46:27 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -4,7 +4,7 @@ class Conexion
     public static function conectar()
     {
         $con = mysqli_init();
-        mysqli_ssl_set($con,NULL,NULL, "SSL/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+        mysqli_ssl_set($con,NULL,NULL, "SSL/DigiCertGlobalRootCA.crt", NULL, NULL);
         mysqli_real_connect($con, "papeleriadb.mysql.database.azure.com", "administrador", "LN123456*", "db_papeleria", 3306, MYSQLI_CLIENT_SSL);
     }
 }
