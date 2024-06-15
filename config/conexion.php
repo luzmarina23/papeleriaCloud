a7125af42d2281bd890bcfe855d58ed63dfa1d42
luzmarina23 <142471155+luzmarina23@users.noreply.github.com>
2024-06-14 22:40:25 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -4,7 +4,7 @@ class Conexion
     public static function conectar()
     {
     $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => 'BaltimoreCyberTrustRoot.crt.pem'
+    PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
     );
     $db = new PDO('mysql:host=papeleriadb.mysql.database.azure.com;port=3306;dbname=db_papeleria', 'administrador', 'LN123456*', $options);
     return $db;
